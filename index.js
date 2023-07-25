b51c4a9d1a971c2585df5ba4413c6c50f43140d1
Renato Bertizini <rbertizini@gmail.com>
2023-07-25 10:10:43 -0300

    Reduzindo Json de retorno

--- index.js
+++ index.js
@@ -1,49 +1,81 @@
-const express = require('express')
-const CircularJSON = require('circular-json');
-const app = express()
-const port = 3000
+const express = require("express");
+const CircularJSON = require("circular-json");
+const app = express();
+const port = 3000;
 
-const axios = require('axios');
+const axios = require("axios");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/oi', (req, res) => {
-	res.send('Hello World!')
-  })
+app.get("/oi", (req, res) => {
+	res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log('Executando webapi')
-})
+	console.log("Executando webapi");
+});
 
+app.get("/api/dadosSAP", async (req, res) => {
+	//Variáveis
+	var response = "";
 
-  app.get('/api/dadosSAP', async (req, res) => {
 	try {
-	  const apiKey = 'Iv5WWPRGgjBHpJjZCOnMGLxrAvDgAzPT'; 
-	  
-	  const apiUrl = 'https://sandbox.api.sap.com/s4hanacloud/sap/opu/odata/sap/API_PRODUCT_SRV/A_Product?$inlinecount=allpages&$top=50';
-	  
-	  // Configuração dos parâmetros e headers na requisição
-	  const options = {		
-		  headers: {			
-			  'APIKey': apiKey,
-			  'DataServiceVersion': '2.0',
-			  'Accept': 'application/json',
-			}, // Indica que a resposta deve ser automaticamente parseada como JSON
-		};
-  
-	  // Fazer a requisição à API do SAP HUB usando o request-promise
-	  const response = await axios.get(apiUrl, options);
-	  console.log (response);
-  
-	  const jsonString = CircularJSON.stringify(response); 	  
-	  console.log(jsonString);
 
-	  res.send(jsonString);
+		//Constantes
+		const apiKey = "Iv5WWPRGgjBHpJjZCOnMGLxrAvDgAzPT";
+		const apiUrl =
+			"https://sandbox.api.sap.com/s4hanacloud/sap/opu/odata/sap/API_PRODUCT_SRV/A_Product?$inlinecount=allpages&$top=50";
+
+		// Configuração dos parâmetros e headers na requisição
+		const options = {
+			headers: {
+				APIKey: apiKey,
+				DataServiceVersion: "2.0",
+				Accept: "application/json",
+			},
+		};
+
+		//Fazer a requisição à API do SAP HUB usando o request-promise
+		response = await axios.get(apiUrl, options);
 	} catch (error) {
-	  console.log(error);
-	  console.error('Erro ao obter os dados do SAP HUB:', error.message);
-	  res.status(500).json({ error: 'Erro ao obter os dados do SAP HUB' });
+		console.log(error);
+		console.error("Erro ao obter os dados do SAP HUB:", error.message);
+		res.status(500).json({ error: "Erro ao obter os dados do SAP HUB" });
 	}
-  });
-  
+	//Como debugar para noobies (eu)
+	//https://www.alura.com.br/artigos/debugando-projetos-nodejs-no-vscode?gclid=Cj0KCQjw5f2lBhCkARIsAHeTvliBK7tK9u28JHYgRwYcghNhX-YrzC4IBwafdVTwgWSarDH0IvFZXLcaAkcwEALw_wcB
+
+	var jsonFiltro = {
+		materials: []
+	};
+
+	//Formatando informação
+	// Criando uma instância da classe
+	const myData = new Materials();
+
+	if (response.data.d.results.length > 0) {
+		for (let i = 0; i < response.data.d.results.length; i++) {
+			myData.addItem(response.data.d.results[i].Product, response.data.d.results[i].ProductType);						
+		}
+	}
+
+	// Convertendo a instância para JSON
+	const jsonRet = myData.toJSON();
+	res.send(jsonRet);
+});
+
+class Materials {
+	constructor() {
+		this.Item = [];
+	}
+
+	addItem(id, type) {
+		const novoMat = { id, type };
+		this.Item.push(novoMat);
+	}
+
+	toJSON() {		
+		return JSON.stringify({ Item: this.Item });
+	}
+}
